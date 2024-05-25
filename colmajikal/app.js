55a198473390b2b41c97c483e8c3e0d4c6507d11
Omarko65 <okormark@gmail.com>
2024-05-25 01:46:29 +0100

    files

---
+++ colmajikal/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'colmajikal', 'index.html'))
+// });
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'))
+})
+
+app.get('/accessories', (req, res) => {
+    res.sendFile(path.join(__dirname, 'accessories.html'))
+})
+
+app.get('/computer', (req, res) => {
+    res.sendFile(path.join(__dirname, 'computing.html'))
+})
+
+app.get('/electronics', (req, res) => {
+    res.sendFile(path.join(__dirname, 'electronics.html'))
+})
+
+app.get('/phones-and-tablets', (req, res) => {
+    res.sendFile(path.join(__dirname, 'phones-and-tables.html'))
+})
+
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, '/customer/account/index.html'))
+})
+
+app.get('/contact-us', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact/index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on PORT 3000')
+})
